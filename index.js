f0e9faf349e0055c764a7850c524c6c05dbe74b2
Annette0000 <55258543+Annette0000@users.noreply.github.com>
2020-05-06 06:50:54 +0300

    portadded

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ const server = new ApolloServer({
   resolvers
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`ð Server ready at ${url}`);
 });
